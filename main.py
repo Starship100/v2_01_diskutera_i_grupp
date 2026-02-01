28e22f62cad0a6d717fa0db0dd25761fb2415624
Starship100 <jimmy.widlund@outlook.com>
2026-02-01 01:20:57 +0100

    more conditions

--- main.py
+++ main.py
@@ -19,22 +19,29 @@ print("Efter rabatter blir priset.... " + str(final_price))
 """""
 
 is_member = input("Are you a member? (y/n)")
-if is_member == "y":
-    print("Great! You are aloud to have discounts on your next bye")
-elif is_member == "n":
-    print("No discounts for you!")
-
 level1 = 100
 level2 = 300
 discount = 0
 
-price = float(input("Välkommen, köp något dyrt: "))
-if price >= level1: #and < level2:
-    print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
-    discount = discount + 10
-if price >= level2:
-    print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
-    discount = discount + 25
+if is_member == "y":
+    print("Great! You are aloud to have discounts on your next bye")
 
-final_price = int(price * (100 - discount) / 100)
-print("Efter rabatter blir priset.... " + str(final_price) + "kr")
+    price = float(input("Välkommen, köp något dyrt: "))
+    if price >= level2:  # and < level2:
+        print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
+        discount = discount + 25
+        final_price = int(price * (100 - discount) / 100)
+        print("Efter rabatter blir priset.... " + str(final_price) + "kr")
+    elif price >= level1:
+        print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
+        discount = discount + 10
+        final_price = int(price * (100 - discount) / 100)
+        print("Efter rabatter blir priset.... " + str(final_price) + "kr")
+    elif price < level1:
+        print("Priset blir", price,"kronor")
+
+    #final_price = int(price * (100 - discount) / 100)
+    #print("Efter rabatter blir priset.... " + str(final_price) + "kr")
+elif is_member == "n":
+    print("No discounts for you!")
+
