34967d3a608d2f791e11a0cd0c41c2c0164b9d79
Starship100 <jimmy.widlund@outlook.com>
2026-02-03 13:18:09 +0100

    Miniräknare

--- v2_05_Miniräknare.py
+++ v2_05_Miniräknare.py
@@ -12,11 +12,12 @@ elif tal_1 == tal_2 or tal_2 == tal_3 or tal_1 == tal_3:
 
 if tal_1 > tal_2 > tal_3 or tal_1 < tal_2 < tal_3:
     print("Det mellersta talet är: " + str(tal_2))
-
 elif tal_3 > tal_1 > tal_2 or tal_3 < tal_1 < tal_2:
     print("Det mellersta talet är " + str(tal_1))
-else:
+elif tal_1 > tal_3 > tal_2 or tal_1 < tal_3 < tal_2:
     print("Det mellersta talet är " + str(tal_3))
+else:
+    print("Det finns inget mellersta tal.")
 
 if tal_2 < tal_1 > tal_3:
     print("Talet " + str(tal_1) + " är störst.")
