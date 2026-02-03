e4edafe8ee7bb7b94934a368de89bc207f51d034
Starship100 <jimmy.widlund@outlook.com>
2026-02-03 13:11:46 +0100

    Miniräknare

---
+++ v2_05_Miniräknare.py
@@ -0,0 +1,26 @@
+
+tal_1 = int(input("Skriv in ett tal: "))
+tal_2 = int(input("Skriv in ett andra tal: "))
+tal_3 = int(input("Skriv in ett tredje tal: "))
+summa = tal_1 + tal_2 + tal_3
+print("Summan blir: " + str(summa))
+
+if tal_1 == tal_3 == tal_2:
+    print("Alla tre tal är lika.")
+elif tal_1 == tal_2 or tal_2 == tal_3 or tal_1 == tal_3:
+    print("Två av talen är samma.")
+
+if tal_1 > tal_2 > tal_3 or tal_1 < tal_2 < tal_3:
+    print("Det mellersta talet är: " + str(tal_2))
+
+elif tal_3 > tal_1 > tal_2 or tal_3 < tal_1 < tal_2:
+    print("Det mellersta talet är " + str(tal_1))
+else:
+    print("Det mellersta talet är " + str(tal_3))
+
+if tal_2 < tal_1 > tal_3:
+    print("Talet " + str(tal_1) + " är störst.")
+elif tal_1 < tal_2 > tal_3:
+    print("Talet " + str(tal_2) + " är störst.")
+else:
+    print("Talet " + str(tal_3) + " är störst.")
